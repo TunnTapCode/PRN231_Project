d37fa6ffd664d84085569a2e2445598526451af0
Nguyễn Quốc Tuấn <129683215+TunnTapCode@users.noreply.github.com>
2024-07-14 09:24:30 +0700

    aa

--- EventDB.sql
+++ EventDB.sql
@@ -1,4 +1,4 @@
-﻿create database EventDB ;
+﻿--create database EventDB ;
 use EventDB ;
 
 -- Tạo bảng Users
--- PRN231_Project_QLSK/client_web_qlsk/obj/Debug/net6.0/client_web_qlsk.AssemblyInfo.cs
+++ PRN231_Project_QLSK/client_web_qlsk/obj/Debug/net6.0/client_web_qlsk.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("client_web_qlsk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a50e8bbc6f612bcefe7571ca4fa1ced049185649")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfb79c29effbbe2f5e932abe185c9948a04fe8cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("client_web_qlsk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("client_web_qlsk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
