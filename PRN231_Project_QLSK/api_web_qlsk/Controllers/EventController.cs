ee5fb9849891947f8cdc5f6051c964706a7618e5
TunnTapCode <hoangtuan69a1@gmail.com>
2024-07-15 12:27:58 +0700

    aa

--- PRN231_Project_QLSK/api_web_qlsk/Controllers/EventController.cs
+++ PRN231_Project_QLSK/api_web_qlsk/Controllers/EventController.cs
@@ -1,7 +1,10 @@
 ﻿using api_web_qlsk.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace api_web_qlsk.Controllers
 {
@@ -13,6 +16,7 @@ namespace api_web_qlsk.Controllers
 
 
         private readonly EventDBContext db = new EventDBContext();
+        private readonly string _storagePath = Path.Combine(Directory.GetCurrentDirectory(), "Uploads");
 
         [HttpGet("getTop12")]
 
@@ -21,8 +25,8 @@ namespace api_web_qlsk.Controllers
 
             try
             {
-                    var events = db.Events.Where(x => x.User.Username == username && x.EndTime > DateTime.Now || x.EndTime == null).OrderBy(x => x.StartTime).Take(12).ToList();
-                    return Ok(events);
+                var events = db.Events.Where(x => x.User.Username == username && x.EndTime > DateTime.Now || x.EndTime == null).OrderBy(x => x.StartTime).Take(12).ToList();
+                return Ok(events);
             }
             catch (Exception ex)
             {
@@ -47,5 +51,74 @@ namespace api_web_qlsk.Controllers
 
 
         }
+        [HttpPost]
+
+        public async Task<IActionResult> SaveEventAsync([FromForm] IFormCollection formData)
+        {
+            // Lấy các giá trị từ formData
+            string title = formData["title"];
+            IFormFile anhImg = formData.Files["image"];
+            string location = formData["location"];
+            string startDate = formData["startDate"];
+            string endDate = formData["endDate"];
+            string description = formData["description"];
+            string username = formData["username"];
+            string scheduleJson = formData["schedule"];
+            var schedule = JsonSerializer.Deserialize<List<Schedule>>(scheduleJson);
+
+            var user = db.Users.FirstOrDefault(x => x.Username.Equals(username));
+
+            var even = new Event();
+            even.Title = title;
+            even.Location = location;
+            even.StartTime = DateTime.Parse(startDate);
+            even.EndTime = DateTime.Parse(endDate);
+            even.Description = description;
+            even.CreatedAt = DateTime.Now;
+            even.UserId = user.UserId;
+            db.Events.Add(even);
+            db.SaveChanges();
+
+
+            if (!Directory.Exists(_storagePath))
+            {
+                Directory.CreateDirectory(_storagePath);
+            }
+
+            string imagePath = null;
+            if (anhImg != null && anhImg.Length > 0)
+            {
+                string uniqueFileName = Path.GetRandomFileName() + Path.GetExtension(anhImg.FileName);
+                imagePath = Path.Combine(_storagePath, uniqueFileName);
+
+                using (var stream = new FileStream(imagePath, FileMode.Create))
+                {
+                    await anhImg.CopyToAsync(stream);
+                }
+                even.Image = imagePath;
+                db.SaveChanges();
+
+            }
+
+            foreach (var s in schedule)
+            {
+                var Schedule = new Schedule();
+                Schedule.Title = s.Title;
+                Schedule.Location = s.Location;
+                Schedule.StartTime = s.StartTime;
+                Schedule.EndTime = s.EndTime;
+                Schedule.CreatedAt = DateTime.Now;
+                Schedule.EventId = even.EventId;
+                Schedule.Description = s.Description;
+
+                db.Schedules.Add(Schedule);
+                db.SaveChanges();
+
+            }
+
+            return Ok(new { success = true, message = "Data received successfully" });
+
+        }
+
     }
 }
--- PRN231_Project_QLSK/api_web_qlsk/obj/Debug/net6.0/api_web_qlsk.AssemblyInfo.cs
+++ PRN231_Project_QLSK/api_web_qlsk/obj/Debug/net6.0/api_web_qlsk.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api_web_qlsk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfb79c29effbbe2f5e932abe185c9948a04fe8cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d37fa6ffd664d84085569a2e2445598526451af0")]
 [assembly: System.Reflection.AssemblyProductAttribute("api_web_qlsk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api_web_qlsk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
